e7f9e00ea1bacc40c3adb4948907524304ace5ab
Dominik Surma <dominiksurma04@gmail.com>
2023-04-06 12:53:48 +0200

    Added input text area for switching pages

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,39 +1,47 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import usePostStore from "../hooks/usePosts";
 
 function Posts() {
   const AUTH_TOKEN = localStorage.getItem("token");
   axios.defaults.headers.common["Authorization"] = "Bearer " + AUTH_TOKEN;
-  const params = {
-    category: "1a2996e6-98d5-49c4-8619-397f95645325",
-    page: 1,
-    size: 50,
-  };
+
   const posts = usePostStore((state) => state.posts);
   const setPosts = usePostStore((state) => state.setPosts);
+
+  const [pageCount, setPageCount] = useState(1);
+
   function loadPosts() {
     axios
-      .get(process.env.REACT_APP_DATABASE_IP + "/posts", params)
+      .get(process.env.REACT_APP_DATABASE_IP + "/posts", {
+        params: { page: pageCount },
+      })
       .then((response) => {
         let postsArray = response.data.items.reverse();
         postsArray = postsArray.filter((item) => item.disabled === false);
         setPosts(postsArray);
       });
   }
+  useEffect(loadPosts, []);
+  useEffect(loadPosts, [pageCount]);
+
   function deletePost(postID) {
     axios
       .delete(process.env.REACT_APP_DATABASE_IP + "/posts?post_id=" + postID)
       .then((response) => {
-        console.log(response);
         loadPosts();
       });
   }
-  //eslint-disable-next-line
-  useEffect(loadPosts, []);
-
   return (
     <>
+      <input
+        type="number"
+        name="pageCount"
+        value={pageCount}
+        onChange={(e) => {
+          setPageCount(e.target.value);
+        }}
+      />
       {posts.map((post) => (
         <div className="Post mx-auto" key={post.id}>
           <div className="post-title">{post.title}</div>
